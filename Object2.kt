4b9ba32bac3c38b78996fc60e3289dde223a3310
milkteahot <greenegoode11@gmail.com>
2020-06-16 09:59:00 +0900

    Object2

---
+++ Object2.kt
@@ -0,0 +1,27 @@
+//companion 오브젝트
+fun main(){
+   var a = FoodPoll("짜장")
+   var b = FoodPoll("짬뽕")
+   
+   a.vote()
+   a.vote()
+   
+   b.vote()
+   b.vote()
+   b.vote()
+   
+   println("${a.name}: ${a.count}")
+   println("${b.name}: ${b.count}")
+   println("총계: ${FoodPoll.total}")
+}
+
+class FoodPoll(val name:String){
+    companion object {
+        var total = 0
+    }
+    var count = 0
+    fun vote(){
+        total++
+        count++
+    }
+}
